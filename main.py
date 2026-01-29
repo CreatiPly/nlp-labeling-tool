30a94a8537482ef23b1457ec73a83d5325448b09
Haseeb Khan <hk2863262@gmail.com>
2026-01-29 17:59:12 +0500

    experimenting with borders and reliefs to seprate the sections visually

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ nlp_text_labeling_tool.rowconfigure(1, weight=1)
 
 #LEFT SIDE BAR
 
-frame_left = Frame(nlp_text_labeling_tool, background="blue")
+frame_left = Frame(nlp_text_labeling_tool, background="#2d2d2d", relief="groove", borderwidth=2)
 frame_left.grid(row=0, column=0, rowspan=2, sticky="nsew")
 
 frame_left.columnconfigure(0, weight=1)
@@ -23,19 +23,19 @@ frame_left.rowconfigure(0, weight=1)
 frame_left.rowconfigure(1, weight=0)
 
 select_file = Button(frame_left, text="Select File")
-select_file.grid(row=1, column=0, sticky="ew")
+select_file.grid(row=1, column=0, sticky="ew", pady=10, padx=10)
 
 # MIDDLE SECTION
 
-frame_mid = Frame(nlp_text_labeling_tool, background="red")
+frame_mid = Frame(nlp_text_labeling_tool, background="#2d2d2d")
 frame_mid.grid(row=0, column=1, sticky="nsew", rowspan=2)
 
 frame_mid.columnconfigure(0, weight=1)
 frame_mid.rowconfigure(0, weight=1)
 frame_mid.rowconfigure(1, weight=0)
 
-button_frame = Frame(frame_mid, background="lightgray")
-button_frame.grid(row=1, column=0, sticky="ew")
+button_frame = Frame(frame_mid,)
+button_frame.grid(row=1, column=0, sticky="ew", pady=10, padx=10)
 
 button_frame.columnconfigure(0, weight=1)
 button_frame.columnconfigure(1, weight=1)
@@ -50,12 +50,12 @@ delete_label.grid(row=0, column=2, sticky="ew")
 
 # RIGHT SIDE BAR : UP
 
-frame_right_up = Frame(nlp_text_labeling_tool, background="green")
+frame_right_up = Frame(nlp_text_labeling_tool, background="#2d2d2d")
 frame_right_up.grid(row=0, column=2, sticky="nsew")
 
 # RIGHT SIDE BAR : DOWN
 
-frame_right_down = Frame(nlp_text_labeling_tool, background="yellow")
+frame_right_down = Frame(nlp_text_labeling_tool, background="#2d2d2d")
 frame_right_down.grid(row=1, column=2, sticky="nsew")
 
 nlp_text_labeling_tool.mainloop()
