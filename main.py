6204b1aafbe14f462fd063f6f21325e67dc6cab4
Haseeb Khan <hk2863262@gmail.com>
2026-01-30 09:20:31 +0500

    found out about LabelFrames they add heading to a section by default so switched to those, layout complete

--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 from tkinter import *
 
 nlp_text_labeling_tool = Tk()
-nlp_text_labeling_tool.geometry("1200x600")
+nlp_text_labeling_tool.geometry("1280x720")
 nlp_text_labeling_tool.title("NLP Text Labeling Tool by Haseeb Khan 539657")
+nlp_text_labeling_tool.configure(background="#2d2d2d")
 
 nlp_text_labeling_tool.columnconfigure(0, weight=1)
 nlp_text_labeling_tool.columnconfigure(1, weight=3)
@@ -15,58 +16,76 @@ nlp_text_labeling_tool.rowconfigure(1, weight=1)
 
 # LEFT SIDE BAR
 
-frame_left = Frame(
-    nlp_text_labeling_tool, background="#2d2d2d", relief="groove", borderwidth=2
+frame_left = LabelFrame(
+    nlp_text_labeling_tool,
+    text="  Files   ",
+    fg="#ffffff",
+    background="#2d2d2d",
+    padx=10,
+    pady=10,
 )
-frame_left.grid(row=0, column=0, rowspan=2, sticky="nsew")
+frame_left.grid(row=0, column=0, rowspan=2, sticky="nsew", padx=5, pady=5)
 
 frame_left.columnconfigure(0, weight=1)
 frame_left.rowconfigure(0, weight=1)
 frame_left.rowconfigure(1, weight=0)
 
 select_file = Button(frame_left, text="Select File")
-select_file.grid(row=1, column=0, sticky="ew", pady=10, padx=10)
+select_file.grid(row=1, column=0, sticky="ew")
 
 # MIDDLE SECTION
 
-frame_mid = Frame(
-    nlp_text_labeling_tool, background="#2d2d2d", relief="groove", borderwidth=2
+frame_mid = LabelFrame(
+    nlp_text_labeling_tool,
+    text="  Text Display  ",
+    fg="#ffffff",
+    background="#2d2d2d",
+    padx=10,
+    pady=10,
 )
-frame_mid.grid(row=0, column=1, sticky="nsew", rowspan=2)
+frame_mid.grid(row=0, column=1, sticky="nsew", rowspan=2, padx=5, pady=5)
 
 frame_mid.columnconfigure(0, weight=1)
 frame_mid.rowconfigure(0, weight=1)
 frame_mid.rowconfigure(1, weight=0)
 
-button_frame = Frame(
-    frame_mid,
-)
-button_frame.grid(row=1, column=0, sticky="ew", pady=10, padx=10)
+button_frame = Frame(frame_mid, background="#2d2d2d")
+button_frame.grid(row=1, column=0, sticky="ew")
 
 button_frame.columnconfigure(0, weight=1)
 button_frame.columnconfigure(1, weight=1)
 button_frame.columnconfigure(2, weight=1)
 
 add_label = Button(button_frame, text="Add Label")
-add_label.grid(row=0, column=0, sticky="ew")
+add_label.grid(row=0, column=0, sticky="ew", padx=5)
 edit_label = Button(button_frame, text="Edit Label")
-edit_label.grid(row=0, column=1, sticky="ew")
+edit_label.grid(row=0, column=1, sticky="ew", padx=5)
 delete_label = Button(button_frame, text="Delete Label")
-delete_label.grid(row=0, column=2, sticky="ew")
+delete_label.grid(row=0, column=2, sticky="ew", padx=5)
 
 # RIGHT SIDE BAR : UP
 
-frame_right_up = Frame(
-    nlp_text_labeling_tool, background="#2d2d2d", relief="groove", borderwidth=2
+frame_right_up = LabelFrame(
+    nlp_text_labeling_tool,
+    text="  Annotations  ",
+    fg="#ffffff",
+    background="#2d2d2d",
+    padx=10,
+    pady=10,
 )
-frame_right_up.grid(row=0, column=2, sticky="nsew")
+frame_right_up.grid(row=0, column=2, sticky="nsew", padx=5, pady=5)
 
 # RIGHT SIDE BAR : DOWN
 
-frame_right_down = Frame(
-    nlp_text_labeling_tool, background="#2d2d2d", relief="groove", borderwidth=2
+frame_right_down = LabelFrame(
+    nlp_text_labeling_tool,
+    text="  Stats  ",
+    fg="#ffffff",
+    background="#2d2d2d",
+    padx=10,
+    pady=10,
 )
-frame_right_down.grid(row=1, column=2, sticky="nsew")
+frame_right_down.grid(row=1, column=2, sticky="nsew", padx=5, pady=5)
 
 frame_right_down.columnconfigure(0, weight=1)
 frame_right_down.rowconfigure(0, weight=1)
@@ -74,6 +93,6 @@ frame_right_down.rowconfigure(1, weight=0)
 
 
 save_labels = Button(frame_right_down, text="Save Labels")
-save_labels.grid(row=1, column=0, sticky="ew", pady=10, padx=10)
+save_labels.grid(row=1, column=0, sticky="ew")
 
 nlp_text_labeling_tool.mainloop()
